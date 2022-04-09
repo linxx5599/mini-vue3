72b1bcc4c3127ac6fe4fec25e3fbc47d9ead008e
linxx5599 <linxx5599@163.com>
2022-04-09 21:47:23 +0800

    添加 readonly 只读 不可修改

--- src/reactivity/restive.ts
+++ src/reactivity/restive.ts
@@ -8,6 +8,20 @@ export function reactive(raw) {
   });
 }
 
+export function readonly(raw) {
+  return new Proxy(raw, {
+    get,
+    set(target, key: any, value) {
+      console.warn(
+        `key:${key} set 失败 因为 target 是 readonly`,
+        target,
+        `value: ${value}`
+      );
+      return true;
+    }
+  });
+}
+
 function createGetter() {
   return function get(target, key) {
     const res = Reflect.get(target, key);
--- src/reactivity/tests/reactivity.spec.ts
+++ src/reactivity/tests/reactivity.spec.ts
@@ -1,9 +1,19 @@
-import { reactive } from "../restive";
+import { reactive, readonly } from "../restive";
 describe("reactive", () => {
   it("happy path", () => {
     const testObj = { vmName: "vmware", detailsList: [1, 2, 3] };
     const reactiveTestObj = reactive(testObj);
     expect(reactiveTestObj).not.toBe(testObj);
     expect(reactiveTestObj.vmName).toBe("vmware");
+  });
+
+  it("readonly warn then call set", () => {
+    console.warn = jest.fn();
+    const user = readonly({
+      age: 10
+    });
+    user.age = 11;
+    expect(user.age).toBe(10);
+    expect(console.warn).toBeCalled();
   });
 });
