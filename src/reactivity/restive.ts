2c84a20c7fb4ccabc941070bbf01e952a8f3f1b7
linxx5599 <linxx5599@163.com>
2022-04-09 21:35:13 +0800

    提取公共代码 重构

--- src/reactivity/restive.ts
+++ src/reactivity/restive.ts
@@ -1,12 +1,23 @@
+const get = createGetter();
+const set = createSetter();
+
 export function reactive(raw) {
   return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-      return res;
-    }
+    get,
+    set
   });
+}
+
+function createGetter() {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+    return res;
+  };
+}
+
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+    return res;
+  };
 }
